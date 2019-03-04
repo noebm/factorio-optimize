58262d4973da3cc1880a1a4129cfbd746d3963f5
noebm <moritz.noebauer@gmail.com>
2019-03-04 02:33:51 +0100

    Refactored factory and added requiredThroughput

--- src/Factory.hs
+++ src/Factory.hs
@@ -2,13 +2,14 @@ module Factory
 where
 
 import Prelude hiding (product)
-import Data.Foldable (toList, find)
-import Control.Arrow (first, second)
-import Data.Either -- (rights)
+import Data.Foldable (toList)
+import Control.Arrow (second)
 import Data.List (intercalate)
 import Data.Maybe (maybeToList)
 import Data.Map (Map , fromList)
 import qualified Data.Map as Map
+
+import Data.Ratio
 
 import Ratio
 import Recipe
@@ -28,12 +29,25 @@ factoryProduct f = product (worker f)
 scaleFactory :: Word -> Factory -> Factory
 scaleFactory s f = f { workerCount = s * workerCount f , inputs = second (s *) <$> inputs f }
 
+{-
+  bandwidth requirement for external items
+-}
 requiredInputs :: Factory -> [ (Item, Ratio Word) ]
 requiredInputs f = Map.toList $ Map.unionsWith (+) $ fmap (fromList . return) $ do
   (item , itemsPerSecond) <- ingredientsPerSecond (worker f)
   case inputs f Map.!? item of
     Just (f' , mult) -> second (fromIntegral mult *) <$> requiredInputs f'
     Nothing -> return (item, itemsPerSecond * fromIntegral (workerCount f))
+
+{-
+  bandwidth requirement including intermediate steps
+-}
+requiredThroughput :: Factory -> [ (Item, Ratio Word) ]
+requiredThroughput f = Map.toList $ Map.unionsWith (+) $ fmap (fromList . return) $ do
+  x <- ingredientsPerSecond (worker f)
+  (:) (second (fromIntegral (workerCount f) *) x) $ do
+    (f', mult) <- maybeToList . (inputs f Map.!?) $ fst x
+    second (fromIntegral mult *) <$> requiredThroughput f'
 
 {-
   finds the optimal ratio producing the item
@@ -43,16 +57,17 @@ requiredInputs f = Map.toList $ Map.unionsWith (+) $ fmap (fromList . return) $ 
 solveChain :: Item -> [ Recipe ] -> Maybe Factory
 solveChain item context = do
   outputRecipe <- findProduct item context
-
-  let (factor , factors)
-        = first (foldl lcmRatio 1) . unzip
-        . fmap (\ (Right f, ips) -> (lcmRatio ips (factoryOutputPerSecond f) / ips, (ips , f)))
-        . filter (isRight . fst)
-        . fmap (first (\ item' -> maybe (Left item') Right $ solveChain item' context))
-        $ ingredientsPerSecond outputRecipe
+  let factors = do
+         (item' , ips) <- ingredientsPerSecond outputRecipe
+         f <- maybeToList $ solveChain item' context
+         return (f , ips)
+  let factor
+        = foldl lcmRatio 1
+        . fmap (\(f , ips) -> lcmRatio ips (factoryOutputPerSecond f) / ips)
+        $ factors
   return $ Factory
     { inputs = fromList $ do
-        (ips, f) <- factors
+        (f , ips) <- factors
         let ops = factoryOutputPerSecond f
         return $ (,) (factoryProduct f) (f , ratioToIntegral (ips / ops * factor))
     , worker = outputRecipe
@@ -62,8 +77,8 @@ solveChain item context = do
 simplFactoryShow :: Factory -> String
 simplFactoryShow f = unlines (aux f) where
   aux :: Factory -> [ String ]
-  aux f =
-    [ intercalate "\n" $ (replicate 2 ' ' ++) <$> aux (scaleFactory scale f)
-    | (f , scale) <- toList (inputs f) ]
+  aux f' =
+    [ intercalate "\n" $ (replicate 2 ' ' ++) <$> aux (scaleFactory scale f'')
+    | (f'' , scale) <- toList (inputs f') ]
     ++
-    [ name (product (worker f)) ++ " x " ++ show (workerCount f)]
+    [ name (product (worker f')) ++ " x " ++ show (workerCount f')]
