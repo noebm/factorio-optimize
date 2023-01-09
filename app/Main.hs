f4e5fafbc183a5e216a0f4b3ec3ef2e4180c97c0
noebm <moritz.noebauer@gmail.com>
2023-01-09 17:15:51 +0100

    Cleanup Main

--- app/Main.hs
+++ app/Main.hs
@@ -21,11 +21,10 @@ printSolution itemName recipes = do
   let Just tree = recipeTree aux (Item itemName)
   let factory = scaleRecipeTree tree
     -- optimalFactory (Item itemName) (toList recipesNoOre)
-  printf "Solution for %s:\n" itemName
+  let treeString = drawTree $ fmap branchNameCount factory
+  printf "Solution for %s:\n\n%s\n" itemName treeString
 
-  putStrLn $ drawTree $ fmap branchNameCount factory
   printf "Using recipes:\n"
-
   mapM_ (putStrLn . prettyRecipe) $ factoryRecipes factory
   putStrLn ""
 
@@ -44,34 +43,3 @@ main = do
   printSolution "automation-science-pack" $ toList recipesNoOre
 
   printSolution "logistic-science-pack" $ toList recipesNoOre
-
-{-
-  let itemToRecipe = recipeLookupMap $ toList recipesNoOre
-
-  let Just circuitTree1 = recipeTree itemToRecipe (Item "electronic-circuit")
-  let Just circuitTree2 = recipeTree itemToRecipe (Item "advanced-circuit")
-  let Just tree3 = recipeTree itemToRecipe (Item "copper-cable")
-
-  let circuitTree1' = scaleRecipeTree circuitTree1
-  let circuitTree2' = scaleRecipeTree circuitTree2
-  let tree3' = scaleRecipeTree tree3
-
-  putStrLn $ drawTree $ fmap branchName tree3
-  putStrLn $ drawTree $ fmap branchNameCount tree3'
-
-  putStrLn $ drawTree $ fmap branchName circuitTree1
-  putStrLn $ drawTree $ fmap branchNameCount circuitTree1'
-
-  putStrLn $ drawTree $ fmap branchName circuitTree2
-  putStrLn $ drawTree $ fmap branchNameCount circuitTree2'
-
-  -- mapM_ (putStrLn . drawTree . branchName)
-  --   $ recipeTree itemToRecipe (Item "electronic-circuit")
-
-  -- let aux :: Tree Recipe -> [[String]]
-  --     aux = take 5 . levels . branchName
-
-  -- mapM_ (putStrLn . unlines . fmap unwords . aux)
-  -- mapM_ (putStrLn . drawTree . branchName)
-  --   $ recipeTree itemToRecipe (Item "advanced-circuit")
--}
