ec4a7e0cbaf08570dc20e50bc79d634353a6d5f6
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-07-15 00:45:57 +0200

    add substituters to flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+
   outputs =
     {
       self,
